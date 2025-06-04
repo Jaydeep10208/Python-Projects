9e5e562b6445a001bc99c94564d586c57deee45c
Jaydeep10208 <mishra.20810@gmail.com>
2025-06-04 15:43:58 +0530

    Create password_creater.py

---
+++ Pass word gennrator/password_creater.py
@@ -0,0 +1,12 @@
+import random
+import string
+
+def generate_password(length=12):
+    characters = string.ascii_letters + string.digits 
+    
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+if __name__ == "__main__":
+    length = int(input("Enter your password length :) "))
+    print("Your password is: ", generate_password(length))
